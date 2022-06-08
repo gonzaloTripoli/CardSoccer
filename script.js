358dd3b8ca00883b06140167b8a2628374557206
Gonzalo Tripoli <106360064+gonzaloTripoli@users.noreply.github.com>
2022-06-07 23:16:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,76 @@
-//falta menos pero falta, el "partido" voy a tardar unas horitas en poder hacerlo, me faltan un par de pantallas y creo que nada mas
-//va tomando color de a poco
-
-class App {
+class App { //maneja todas las clases.
     constructor() {
-        this.listUser = []
+        this.listUser = [] //usuario principal se maneja siempre con el indice 0
         this.addPcUser()
     }
+    compararPlayers(enemy, user, marcador) {
+        let a = this.listUser[0].player(user)
+        let b = this.listUser[1].player(enemy)
+        marcador.puntajeEnemigo = a < b ? marcador.puntajeEnemigo : marcador.puntajeEnemigo += 1
+        marcador.puntajeUser = a < b ? marcador.puntajeUser += 1 : marcador.puntajeUser
+        setTimeout(() => {
+            this.renderMarcador()
+        }, 1900);
+
+        return a > b ? user : enemy;
+    }
+    playersOff(listNames) {
+        for (const name of listNames) {
+           let a = document.getElementById(name + "id")
+           a.className ="playerOff col"
+           let b = document.getElementById(name);
+           a.removeChild(b);
+        }
+    }
+    renderMarcador() {
+        let markABorrar = document.getElementById("marcador")
+
+        let c = document.getElementById("game")
+        c.removeChild(markABorrar)
+        let d = document.createElement("h1")
+        d.id = "marcador"
+        d.innerText = "Marcador " + marcador.puntajeEnemigo + " : " + marcador.puntajeUser
+        c.appendChild(d);
+    }
+    setWinner(enemy, user) {
+        this.listUser[1].deletePlayerTeamUser(enemy)
+        let main = document.getElementById("main1")
+        let match = document.getElementById("animation2")
+        setTimeout(() => {
+            main.removeChild(match)
+        }, 3000)
+    }
+
     renderEnemy() {
-        // next
+        let a = this.listUser[1].playerRandom().render();
+        a.className = "playerEnemy col-md-4 .ml-auto"
+        let b = document.getElementById("animation-row")
+        a.style = " margin-left: 430px"
+        b.appendChild(a);
+        let c = document.getElementsByClassName("btn btn-outline-primary")[0]
+        a.removeChild(c)
+
+
+        return a
+
+    }
+    hidePlayers(playerName, marcador) {
+        let num
+        let a = document.getElementById("animation-row")
+        let b = document.getElementsByClassName("col")
+        let i = 0;
+        while (i < b.length) {
+            if (!(b[i].id == playerName + "id")) {
+                a.removeChild(b[i])
+            } else {
+                let c = document.getElementById(playerName);
+                b[i].removeChild(c);
+                num = i;
+                i++;
+            }
+        }
+        b[num].className = "player col-md-4"
+
     }
     movePlayersToTeam(namePlayer) {
         this.listUser[0].movePlayersToTeam(namePlayer)
@@ -17,6 +80,12 @@ class App {
             let game = document.getElementById("main1");
             game.removeChild(document.getElementsByClassName("container")[0]);
             this.renderPlay()
+            let c = document.getElementById("game")
+            let d = document.createElement("h1")
+            d.id = "marcador"
+            d.innerText = "Marcador " + marcador.puntajeEnemigo + " : " + marcador.puntajeUser
+            c.appendChild(d);
+
         }
     }
     previousGame() {
@@ -39,6 +108,7 @@ class App {
         let game = document.getElementById("main1")
         let a = document.createElement("div");
         a.className = "row gy-5"
+        a.id = "animation-row"
         let i = 0
         while (players.cantPlayers() > i) {
             let x = players.playersTeam[i].renderForPlay()
@@ -80,14 +150,13 @@ class App {
     generatePcEnvelope(equipo) {
         for (let i = 0; i < 5; i++) {
             let a = this.listUser.indexOf(this.listUser.find(user => user.teamUser.name == equipo));
-            this.listUser[a].addPlayerToTeam(new Player(("jugador" + i), "Boca"));
+            this.listUser[a].addPlayerToTeam(new Player(("player" + i), "Boca"));
         }
     }
     addUser() {
         let nameUser = document.getElementById("name");
         this.listUser.unshift(new User(nameUser.value));
         this.generateUserEnvelope(9);
-        console.log(this.listUser);
         let name = document.getElementById("game");
         let a = document.createElement("div")
         a.className = "game";
@@ -170,15 +239,18 @@ class App {
             })
         })
     }
-    play() {
-        let ganoUsuario = this.listUser[0].play() > this.listUser[1].play();
-        console.log("Gano  " + (ganoUsuario ? this.listUser[0].getName() : this.listUser[1].getName()));
-    }
+
 }
 class User {
     constructor(name) {
         this.playerUser = [];
         this.teamUser = new TeamUser(name);
+    }
+    player(namePlayer) {
+        return this.teamUser.playerPwr(namePlayer)
+    }
+    playerRandom() {
+        return this.teamUser.playerRandom()
     }
     teamReady() {
         return this.teamUser.isReady();
@@ -194,6 +266,9 @@ class User {
         let index = this.playerUser.indexOf(playerFound);
         this.teamUser.add(this.playerUser.splice(index, 1)[0]);
     }
+    deletePlayerTeamUser(namePlayer) {
+        this.teamUser.deletePlayer(namePlayer);
+    }
 }
 class TeamUser {
     constructor(name) {
@@ -203,18 +278,34 @@ class TeamUser {
     isReady() {
         return this.playersTeam.length < 5;
     }
-    getValue() {
-        let value = 0;
-        this.playersTeam.forEach(player => {
-            value += (player.isDefender() ? player.def : player.atk);
-        })
-        return value;
+    playerPwr(namePlayer) {
+
+        return this.playersTeam.find(player => player.name == namePlayer).playerPwr()
+
+    }
+    deletePlayer(namePlayer) {
+        let player = this.playersTeam.find(player => player.name == namePlayer)
+        let a = this.playersTeam.indexOf(player)
+        this.playersTeam.splice(a, 1)
+        console.log(this.playersTeam)
     }
     add(player) {
         this.playersTeam.push(player);
     }
     cantPlayers() {
         return this.playersTeam.length;
+    }
+    playerRandom() {
+        let cant = this.playersTeam.length;
+        let random
+
+        if (cant > 1) {
+            random = Math.round(Math.random() * (0 - (cant - 1) + 1) + (cant - 1))
+
+        } else {
+            random = 0
+        }
+        return this.playersTeam[random];
     }
 }
 class Player {
@@ -225,20 +316,22 @@ class Player {
         this.def = this.numRandom();
         this.position = this.asignPosition();
     }
-    isDefender() {
-        return this.position == "def";
-    }
     numRandom() {
         return Math.round(Math.random() * (0 - 100 + 1) + 100);
+    }
+    playerPwr() {
+        return this.position == "def" ? this.def : this.atk
     }
     asignPosition() {
         return this.atk > this.def ? "atk" : "def";
     }
+
+    //prometo modularizar y hacerlo bien esto despues....son iguales :(
     render() {
         let name = this.name;
         let a = document.createElement("div")
         a.id = this.name + "id"
-        a.innerHTML = `<h1> ${this.name}</h1>
+        a.innerHTML = `<h1 class = "namePlayer"> ${this.name}</h1>
         <p class  ="position">${this.position}</p>
         <p>Defensa ${this.def}</p>
         <p>Ataque  ${this.atk}</p>
@@ -250,7 +343,7 @@ class Player {
         let name = this.name;
         let a = document.createElement("div")
         a.id = this.name + "id"
-        a.innerHTML = `<h1> ${this.name}</h1>
+        a.innerHTML = `<h1 class = "namePlayer"> ${this.name}</h1>
         <p class  ="position">${this.position}</p>
         <p>Defensa ${this.def}</p>
         <p>Ataque  ${this.atk}</p>
@@ -259,24 +352,28 @@ class Player {
         return a;
     }
 }
-const a = new App();
+
+//____________________________________________________________________________________________________________________________________________________________________________________
+
+//Main
+const a = new App(); //==> Maneja estructura del programa
 let boton = document.getElementById("botonSubmit");
 let enter = document.getElementById("name")
-enter.addEventListener("keyup", function (e) {
-
+enter.addEventListener("keyup", function (e) { //==> permite ingresar con enter
     if (e.key === `Enter`) {
         agregarUsuario()
     }
 })
-// if (localStorage.getItem("user") == null) {
-function agregarUsuario() {
-    a.addUser();
-}
-// } else {
-//     a.addUserPr((localStorage.getItem("user")));
-// }
 
-function pasarJugadorAlEquipo(name) {
+function agregarUsuario() {
+    a.addUser(); // ==> genera el usuario y hace una seguidilla de acciones como crear su equiipo con los 10 jugadores random y renderizar la pantalla
+}
+let marcador = {
+    puntajeEnemigo: 0,
+    puntajeUser: 0,
+}
+
+function pasarJugadorAlEquipo(name) { // ==> movimiento del jugador de el "banco " al equipo titular, a los 5 seleccionados corta
     a.movePlayersToTeam(name.id);
     Toastify({
         gravity: "bottom",
@@ -285,17 +382,42 @@ function pasarJugadorAlEquipo(name) {
         duration: 500
     }).showToast();
 }
+let listNames = []
 
-function playGame(playerName) {
+function playGame(playerName) { // ==> va a contar con la logica y el renderizado de jugar partido, generara un enemigo con 5 jugadores aleatorios el cual por 5 rondas 
     let x = document.getElementById(playerName.id + "id")
-    let bo = true;
-    // if () {
+    let i = 0;
+    a.hidePlayers(playerName.id, marcador)
+    let z = a.renderEnemy() //tirara aleatoriamente un jugador para matchear con el elegido por el usuario
+    let nameEnemy = z.getElementsByClassName("namePlayer")[0].innerText;
+    let nameUser = x.getElementsByClassName("namePlayer")[0].innerText;
+    let win = a.compararPlayers(nameEnemy, nameUser, marcador)
+    let value = win == nameUser ? 500 : -500;
+    let animationId = (win == nameUser ? nameUser : nameEnemy) + "id";
+    let animationIdLoser = (win != nameUser ? nameUser : nameEnemy) + "id";
+    let animationWinner = document.getElementById(animationId)
+    let animationLosser = document.getElementById(animationIdLoser)
     anime({
-        targets: x,
+        targets: animationWinner,
+        translateY: {
+            value: -250,
+            duration: 1500,
+        },
+        translateX: {
+            value: value,
+            delay: 1500,
+        },
+    });
+    anime({
+        targets: animationLosser,
         translateY: -250,
-
         duration: 1900,
     });
-    a.renderEnemy()
-
+    a.setWinner(nameEnemy, nameUser)
+    i++
+    listNames.push(nameUser)
+    setTimeout(() => {
+        a.renderPlay()
+        a.playersOff(listNames)
+    }, 3400)
 }
